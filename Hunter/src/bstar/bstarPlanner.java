aa7a4276e398d0be34c5767d937617e917672bb9
benjytcook <benjytcook@abd69d97-cdb0-11bc-af27-1305a8ba0f8d>
2010-12-21 15:14:11 +0000

    Hunter implementation of pathplanner

---
+++ Hunter/src/bstar/bstarPlanner.java
@@ -0,0 +1,97 @@
+package bstar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+import cz.cuni.amis.pogamut.base.utils.logging.LogCategory;
+import cz.cuni.amis.pogamut.base.agent.navigation.AbstractPathHandle;
+import cz.cuni.amis.pogamut.base.agent.navigation.PathHandle;
+import cz.cuni.amis.pogamut.base.agent.navigation.PathNotConstructable;
+import cz.cuni.amis.pogamut.base.agent.navigation.PathPlanner;
+import cz.cuni.amis.pogamut.base3d.worldview.object.ILocated;
+import cz.cuni.amis.pogamut.ut2004.bot.impl.UT2004Bot;
+import cz.cuni.amis.pogamut.base3d.worldview.object.Location;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.NavPoint;
+import cz.cuni.amis.pogamut.base.utils.math.DistanceUtils;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.Item;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.Player;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.Self;
+
+import org.python.util.PythonInterpreter;
+
+
+// @todo: change runPython from recieving start and to as navpoints
+// to locations.
+// bstarPlanner's responsibility is to change these to closest navs,
+// and reappend the start and to.
+/**
+ *
+ * @author Benjy
+ */
+public class bstarPlanner implements PathPlanner<ILocated, ILocated> {
+
+    UT2004Bot bot;
+    LogCategory user;
+    PythonInterpreter interp;
+    NavPoint[] navs;
+    Player[] players;
+    Item[] items;
+    Self self;
+
+    public bstarPlanner(UT2004Bot bot) {
+        this.bot = bot;
+        this.user = bot.getLogger().getCategory("User");
+        this.navs = new NavPoint[bot.getWorldView().getAll(NavPoint.class).size()];
+        this.players = new Player[bot.getWorldView().getAll(Player.class).size()];
+        this.items = new Item[bot.getWorldView().getAll(Item.class).size()];
+        this.self = bot.getWorldView().getSingle(Self.class);
+        bot.getWorldView().getAll(NavPoint.class).values().toArray(navs);
+        bot.getWorldView().getAll(Player.class).values().toArray(players);
+        bot.getWorldView().getAll(Item.class).values().toArray(items);
+        // construcer / initalization
+        // here we can create our python world
+        PythonInterpreter.initialize(System.getProperties(), System.getProperties(), new String[0]);
+        this.interp = new PythonInterpreter();
+    }
+
+    private Location[] runPython(NavPoint start, NavPoint to) {
+        try {
+            interp.set("start", start);
+            interp.set("to", to);
+            interp.set("navs", navs);
+            interp.set("players", players);
+            interp.set("items", items);
+            interp.set("me", self);
+            interp.execfile("src/bstar/passArguments.py");
+            interp.execfile("src/bstar/astar.py");
+            return interp.get("output", Location[].class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public NavPoint findClosestNavPoint(ILocated pt) {
+        List<NavPoint> navlist = Arrays.asList(navs);
+        NavPoint target = DistanceUtils.getNearest(navlist, pt);
+        return target;
+    }
+
+    public PathHandle<ILocated> computePath(final ILocated to) throws PathNotConstructable {
+        //NavPoint start = DistanceUtils.getNearest(bot.getWorldView().getAllVisible(NavPoint.class).values(), bot.getWorldView().getSingle(Self.class));
+        NavPoint start = findClosestNavPoint(bot.getLocation());
+        NavPoint toNav = findClosestNavPoint(to);
+        // navpoints give us alot
+        Location[] returnedPath;
+        returnedPath = runPython(start, toNav);
+        List<Location> path = new ArrayList(Arrays.asList(returnedPath));
+        path.add(to.getLocation());
+
+        return new AbstractPathHandle<ILocated>(path, true) {
+            public String getTargetDescription() {
+                return to.toString();
+            }
+        };
+    }
+}
---
+++ Hunter/src/bstar/passArguments.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python
+
+__author__="Benjy"
+__date__ ="$Nov 23, 2010 4:54:28 PM$"
+#init graph
+
+def itemToCategory(x):
+    if not x.isInvSpot():
+        return False
+    else:
+        return x.getItemClass().getCategory().toString()
+
+def freeitemToCategory(items):
+    newlist = []
+    for x in items:
+        if x.isDropped():
+            newlist.append([x.getType().getCategory().toString(),x.getLocation()])
+    return newlist
+
+
+# what is passed to python?
+# 1)    The NavGraph - Including Type A Items
+# 2)    Players - locations array
+# 3)    Type B Items
+
+ids = [x.getId() for x in navs]
+neighbors = [x.getOutgoingEdges() for x in navs]
+neighids = [[ids.index(y) for y in [x.getId() for x in neighbors[i].values()]] for i in range(len(navs))]
+navitems = [itemToCategory(x) for x in navs] #HEALTH, AMMO, WEAPON, False
+bitems = freeitemToCategory(items)
+locations = [x.getLocation() for x in navs]
+
+
+
+#graded by planet unreal Weapons Guide
+weaponsGrade = { "AssaultRifle": 2,
+            "ShieldGun": 1,
+            "FlakCannon": 7,
+            "BioRifle": 5,  
+            "ShockRifle": 4,
+            "SniperRifle": 9,
+            "RocketLauncher": 8,
+            "Minigun": 6,
+            "LightingGun": 10,
+            "Translocator": 0}
+
+if len(players)>0:
+    enemies=[x for x in players if x.getLocation()!=None]
+else:
+    enemies=[]
+maxdist = 500
+
+lastItems = items
+
+#TODO: Magic number: What is longest distance between 2 neighboring wp in map?
